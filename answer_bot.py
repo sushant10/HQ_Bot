6c728674e0fecf8914d5778c51723b4ba7885112
sushant10 <sushdude34@gmail.com>
2018-02-10 11:30:22 -0500

    Added sample questions

--- answer_bot.py
+++ answer_bot.py
@@ -1,8 +1,33 @@
-#answering bot for trivia
+#answering bot for trivia HQ and Cash Show
 import urllib2
 import operator
 import json
 import wikipedia
+
+
+#Sample questions from previous games
+sample_questions = {
+						'Who are readers asked to find in the "Where\'s Waldo" books?': 
+							['Michael Bulbe',
+							'Amelia Earhart',
+							'Waldo']
+						'Which of these is a US State?':
+							['Chihuahua'
+							'Saskatchewan' 
+							'Louisiana']
+						'Which of these is a common material used in 3D printers?':
+							['Durocarbon filament',
+							'Polyabsorbic styrene',
+							'Polyactic acid']
+						'Which of these songs does not feature whistling?':
+							['Graveyard Whistling',
+							'Young Folks',
+							'Pumped Up Kicks']
+						'Which NFL great started his pro career with 10 straight losses?':
+							['Brett Favre',
+							'Dan Marino',
+							'Troy Aikman']
+					}
 
 def get_question():
 	return questions, answers
