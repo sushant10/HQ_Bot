23959dd4c8973067560d7f12be3d815a0370a9d8
sushant10 <sushdude34@gmail.com>
2018-02-16 12:30:31 -0500

    OCR Testing

--- answer_bot.py
+++ answer_bot.py
@@ -22,7 +22,7 @@ def load_json():
 	remove_words = json.loads(open("Data/settings.json").read())["remove_words"]
 	sample_questions = json.loads(open("Data/questions.json").read())
 
-# take screenshot of Show
+# take screenshot of question
 def screen_grab(loc):
 	return None
 
@@ -42,27 +42,21 @@ def read_screen():
 	ap.add_argument("-p", "--preprocess", type=str, default="thresh", help="type of preprocessing to be done")
 	args = vars(ap.parse_args())
 
-	# load the example image and convert it to grayscale
+	# load the image 
 	image = cv2.imread(args["image"])
 	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-	# check to see if we should apply thresholding to preprocess the
-	# image
 	if args["preprocess"] == "thresh":
 		gray = cv2.threshold(gray, 0, 255,
 			cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-	# make a check to see if median blurring should be done to remove
-	# noise
 	elif args["preprocess"] == "blur":
 		gray = cv2.medianBlur(gray, 3)
 
-	# write the grayscale image to disk as a temporary file so we can
-	# apply OCR to it
-	filename = "{}.png".format(os.getpid())
+	# store grayscale image as a temp file to apply OCR
+	filename = "Screens/{}.png".format(os.getpid())
 	cv2.imwrite(filename, gray)
-	# load the image as a PIL/Pillow image, apply OCR, and then delete
-	# the temporary file
+	# load the image as a PIL/Pillow image, apply OCR, and then delete the temporary file
 	text = pytesseract.image_to_string(Image.open(filename))
 	os.remove(filename)
 	print(text)
@@ -73,8 +67,8 @@ def read_screen():
 	if cv2.waitKey(0):
 		cv2.destroyAllWindows()
 
-# get questions and options
-def get_question():
+# get questions and options from OCR text
+def parse_question():
 	return questions, answers
 
 
