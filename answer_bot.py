9e1d73a8772bbd11fff8f638cb23cf5f56aaaf93
sushant10 <sushdude34@gmail.com>
2018-02-16 12:20:52 -0500

    test OCR

--- answer_bot.py
+++ answer_bot.py
@@ -16,23 +16,61 @@ sample_questions = {}
 # List of words to clean from the question during google search
 remove_words = []
 
-#load sample questions
+# load sample questions
 def load_json():
 	global remove_words, sample_questions
 	remove_words = json.loads(open("Data/settings.json").read())["remove_words"]
 	sample_questions = json.loads(open("Data/questions.json").read())
 
-#take screenshot of Show
+# take screenshot of Show
 def screen_grab(loc):
 	return None
 
-#parse OCR // pytesseract
+# construct the argument parse and parse the arguments // pytesseract
 def parse_OCR():
-	return None
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--image", required=True,
+	help="path to input image to be OCR'd")
+	ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+	help="type of preprocessing to be done")
+	args = vars(ap.parse_args())
 
 # get questions and options
-def read_screen(screen):
-	return None
+def read_screen():
+	ap = argparse.ArgumentParser(description='HQ_Bot')
+	ap.add_argument("-i", "--image", required=True,help="path to input image to be OCR'd")
+	ap.add_argument("-p", "--preprocess", type=str, default="thresh", help="type of preprocessing to be done")
+	args = vars(ap.parse_args())
+
+	# load the example image and convert it to grayscale
+	image = cv2.imread(args["image"])
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	# check to see if we should apply thresholding to preprocess the
+	# image
+	if args["preprocess"] == "thresh":
+		gray = cv2.threshold(gray, 0, 255,
+			cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+	# make a check to see if median blurring should be done to remove
+	# noise
+	elif args["preprocess"] == "blur":
+		gray = cv2.medianBlur(gray, 3)
+
+	# write the grayscale image to disk as a temporary file so we can
+	# apply OCR to it
+	filename = "{}.png".format(os.getpid())
+	cv2.imwrite(filename, gray)
+	# load the image as a PIL/Pillow image, apply OCR, and then delete
+	# the temporary file
+	text = pytesseract.image_to_string(Image.open(filename))
+	os.remove(filename)
+	print(text)
+ 
+	# show the output images
+	cv2.imshow("Image", image)
+	cv2.imshow("Output", gray)
+	cv2.waitKey(0)
 
 # get questions and options
 def get_question():
@@ -131,5 +169,6 @@ def get_points():
 
 
 if __name__ == "__main__":
-	load_json()
-	get_points()
+	#load_json()
+	#get_points()
+	read_screen()
