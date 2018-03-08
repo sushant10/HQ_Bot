1d5b13921e58a753cf1e19119ebfb465389bbdc7
sushant10 <sushdude34@gmail.com>
2018-03-07 20:53:34 -0500

    GUI Testing

--- GUI_main.py
+++ GUI_main.py
@@ -1,33 +1,39 @@
 import wx
 #Still testing
-
 class HQ_Bot(wx.Frame):
-           
-    def __init__(self, *args, **kw):
-        super(HQ_Bot, self).__init__(*args, **kw) 
-        
-        self.InitUI()
-        
-    def InitUI(self):   
 
-        pnl = wx.Panel(self)
-        #cbtn = wx.Button(pnl, label='Close', pos=(560, 600))
+	def __init__(self, *args, **kw):
+		super(HQ_Bot, self).__init__(*args, **kw)
+
+		self.InitUI()
+
+	def InitUI(self):
+
+		pnl = wx.Panel(self)
+		#cbtn = wx.Button(pnl, label='Close', pos=(560, 600))
 		#cbtn.Bind(wx.EVT_BUTTON, self.OnClose)
 
-        self.SetSize((650, 650))
-        self.SetTitle('wx.Button')
-        self.Centre()
-        self.Show(True)          
-        
-    def OnClose(self, e):
-        self.Close(True)                  
-        
+		cbtn = wx.Button(pnl, label='Live game', pos=(325,325))
+		cbtn.Bind(wx.EVT_BUTTON, self.ans)
+
+
+		self.SetSize((650, 650))
+		self.SetTitle('HQ_Bot')
+		self.Centre()
+		self.Show(True)
+
+	def OnClose(self, e):
+		self.Close(True)
+
+	def ans():
+		return 0
+
 def main():
-    
-    ex = wx.App()
-    HQ_Bot(None)
-    ex.MainLoop()    
+
+	ex = wx.App()
+	HQ_Bot(None)
+	ex.MainLoop()
 
 
 if __name__ == '__main__':
-    main()   
+	main()
